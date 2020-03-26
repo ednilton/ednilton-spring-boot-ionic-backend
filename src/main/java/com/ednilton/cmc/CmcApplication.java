10ffe431914e57037dfbea5bf8751c64ffb60f0a
Ednilton Rauh <edrauh@gmail.com>
2020-03-26 17:27:48 -0300

    Insert data in to database

--- src/main/java/com/ednilton/cmc/CmcApplication.java
+++ src/main/java/com/ednilton/cmc/CmcApplication.java
@@ -3,11 +3,31 @@ package com.ednilton.cmc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CmcApplication {
+import com.ednilton.cmc.domain.Categoria;
+import com.ednilton.cmc.repositories.CategoriaRepository;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+@SpringBootApplication
+public class CmcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CmcApplication.class, args);
 	}
+	
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
+	
 
 }
