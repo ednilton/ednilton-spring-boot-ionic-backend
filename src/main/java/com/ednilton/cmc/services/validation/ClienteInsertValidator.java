6cab71e098b5497b463cf9736a049d8fc9eee57f
Ednilton Rauh <edrauh@gmail.com>
2020-03-30 14:22:28 -0300

    Validação customizada de CPF e CNPJ na inserção de Cliente

---
+++ src/main/java/com/ednilton/cmc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,43 @@
+package com.ednilton.cmc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.ednilton.cmc.domain.enums.TipoCliente;
+import com.ednilton.cmc.dto.ClienteNewDTO;
+import com.ednilton.cmc.resources.exceptions.FieldMessage;
+//import com.ednilton.cmc.services.validation.utils.BR;
+import com.ednilton.cmc.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	/* Verifica se o ClienteNewDTO é valido retornando um boolean*/
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+
+		List<FieldMessage> list = new ArrayList<>();
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+
+		/* ok */
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
