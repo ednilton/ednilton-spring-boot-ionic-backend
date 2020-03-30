f987e4b362e51e5b12ae444effd87f8a462e6412
Ednilton Rauh <edrauh@gmail.com>
2020-03-30 12:59:35 -0300

    Ajuntando e  testando a Deleção de Cliente

--- src/main/java/com/ednilton/cmc/services/ClienteService.java
+++ src/main/java/com/ednilton/cmc/services/ClienteService.java
@@ -72,7 +72,7 @@ public class ClienteService {
 			repo.deleteById(id);
 		}
 		catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+			throw new DataIntegrityException("Não é possível excluir o cliente porque há pedidos relacionados");
 		}
 	}
 
