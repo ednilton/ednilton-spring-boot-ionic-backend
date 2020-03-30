f555fcb747c0e19db900c5c05934a92f9e521020
Ednilton Rauh <edrauh@gmail.com>
2020-03-30 00:39:11 -0300

    Pesonalizando o retorno da Validação

---
+++ src/main/java/com/ednilton/cmc/resources/exceptions/ValidationError.java
@@ -0,0 +1,32 @@
+package com.ednilton.cmc.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+	
+	public ValidationError(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+	
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	/* can add new message errors in to List  */
+	public void addError(String fieldName, String messagem) { 
+	
+			errors.add(new FieldMessage(fieldName, messagem));
+	}
+
+	
+	
+	
+
+
+}
